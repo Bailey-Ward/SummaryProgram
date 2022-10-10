869ca0cc50eeb5ecf0d4e273f0ba10b702c471c3
FirstClassGravy <41171656+FirstClassGravy@users.noreply.github.com>
2022-10-10 21:53:22 +0100

    struct

--- SummaryProgram/SummaryProgram.cpp
+++ SummaryProgram/SummaryProgram.cpp
@@ -16,12 +16,12 @@ int main()
 	char ch;
 	int numberOfWords{}; //int for storing the number of words in the .txt
 	int numberOfSentences{}; //int for storing the number of sentences in the .txt
-	int wordCount = 0; //int for storing the frequency of words in the .txt
-	vector <int> v; //vector for holding the text file
+	//int wordCount = 0; //int for storing the frequency of words in the .txt
+	vector <int> wordList; //vector for holding the text file
 
 	while (getline(ReadFile, textFile)) {
 
-		cout << textFile;
+		cout << textFile + "\n";
 
 		for (int i = 0; i < textFile.length(); i++) {
 			ch = textFile[i];
@@ -38,12 +38,21 @@ int main()
 				numberOfWords++;
 			}
 		}
-		
+
+		struct { //structure declaration
+			string word; //word being counted
+			int count; //occurances of the word
+		} WordFreqPair; //structure variable 
+
 
 	}
 
+	cout << ("\nNumber of sentences:\t ");
 	cout << numberOfSentences;
+	cout << ("\nNumber of words:\t ");
 	cout << numberOfWords;
+
+
 
 
 	ReadFile.close();
